d1f03f6a6b307a970c77509b13191bf12935ea2f
Anmin <Aminschleiter@googlemail.com>
2025-04-06 21:18:13 +0200

    Add requiered changes for azure openai

--- backend/services/ai/azureai.py
+++ backend/services/ai/azureai.py
@@ -1,10 +1,12 @@
 # Implementation for Azure AI
-from langchain_community.llms import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from os import getenv
 
 def get_llm(llm_type: str, llm_model: str, max_tokens: int = 5000):
     if llm_type == "Azure":
         return AzureChatOpenAI(
+            azure_endpoint=getenv("AZURE_OPENAI_ENDPOINT"),
+            openai_api_key=getenv("AZURE_OPENAI_API_KEY"),
             azure_deployment=getenv("AZURE_OPENAI_DEPLOYMENT"),
             api_version=getenv("AZURE_OPENAI_API_VERSION"),
             temperature=0,
