c5917707f1fa47adfd1df651c96f657b606f4d2c
Anmin <Aminschleiter@googlemail.com>
2025-04-19 16:21:05 +0200

    Add Datastate

---
+++ backend/models/states.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+from typing import List
+from .geodata import GeoDataObject  # relativer Import angepasst
+from langgraph.graph import MessagesState  # Passe den Importpfad ggf. an
+
+@dataclass
+class DataState(MessagesState):
+    input: str
+    geodata: List[GeoDataObject] = field(default_factory=list)  
